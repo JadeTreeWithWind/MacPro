16c9f0f6d2d3f67d49fb427089538b7de41af31d
TJ <tj.wang1995@mosji.co>
2025-12-18 15:40:54 +0800

    feat: 新增導覽列，並調整 HTML 標題與語言設定。

---
+++ src/constants/index.js
@@ -0,0 +1,26 @@
+export const navLinks = [
+  {
+    name: '商店',
+    href: '#',
+  },
+  {
+    name: 'Mac',
+    href: '#',
+  },
+  {
+    name: 'iPhone',
+    href: '#',
+  },
+  {
+    name: 'Watch',
+    href: '#',
+  },
+  {
+    name: 'AirPods',
+    href: '#',
+  },
+  {
+    name: '支援',
+    href: '#',
+  },
+]
