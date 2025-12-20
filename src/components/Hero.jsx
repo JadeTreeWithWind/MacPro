6977db9ba7a91ecd61ade6349f81f4cb3bc3c090
TJ <tj.wang1995@mosji.co>
2025-12-20 11:46:16 +0800

    feat: 新增 Hero 元件以展示產品影片與資訊

---
+++ src/components/Hero.jsx
@@ -0,0 +1,34 @@
+import { useEffect, useRef } from "react";
+
+const Hero = () => {
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    if (videoRef.current) {
+      videoRef.current.playbackRate = 2;
+    }
+  }, []);
+
+  return (
+    <section id="hero">
+      <div>
+        <h1>Macbook Pro</h1>
+        <img src="/title.png" alt="Macbook Pro" />
+      </div>
+
+      <video
+        ref={videoRef}
+        src="/videos/hero.mp4"
+        autoPlay
+        playsInline
+        muted
+      ></video>
+
+      <button>購買</button>
+
+      <p>NT$52,900 起</p>
+    </section>
+  );
+};
+
+export default Hero;
