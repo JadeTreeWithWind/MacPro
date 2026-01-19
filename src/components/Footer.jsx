e5d336203f0d2b842f07a4f969a501994d199c0c
TJ <tj.wang1995@mosji.co>
2026-01-19 18:17:00 +0800

    feat: 新增 Footer 元件及其相關連結。

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,41 @@
+import { footerLinks } from "../constants/index.js";
+
 const Footer = () => {
   return (
-    <div>
-      <h1>Footer</h1>
-    </div>
+    <footer>
+      <div className="info">
+        <p>
+          更多選購方式：尋找當地的{" "}
+          <a className="text-blue-500 underline" href="#">
+            Apple 直營店
+          </a>{" "}
+          或其他{" "}
+          <a className="text-blue-500 underline" href="#">
+            零售商
+          </a>
+          ，或致電{" "}
+          <a className="text-blue-500 underline" href="#">
+            0800-020-021
+          </a>
+          。
+        </p>
+        <img src="/logo.svg" alt="Apple logo" />
+      </div>
+
+      <hr />
+
+      <div className="links">
+        <p>Copyright © 2025 Apple Inc. 保留一切權利。</p>
+
+        <ul>
+          {footerLinks.map(({ label, link }) => (
+            <li key={label}>
+              <a href={link}>{label}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </footer>
   );
 };
-
 export default Footer;
